a69d54b4a8bf3c2e898e21b9e9817a83be06d7ba
Ábel Miklós <abelmiklos@outlook.hu>
2023-07-19 23:39:13 +0200

    created project

---
+++ I4P - interju feladat - 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4P___interju_feladat___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
