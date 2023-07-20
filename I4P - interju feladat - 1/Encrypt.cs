59772ea9fde7de5318be317922e67a00e4c0c691
Ábel Miklós <abelmiklos@outlook.hu>
2023-07-21 00:46:55 +0200

    EncryptMsg method

--- I4P - interju feladat - 1/Encrypt.cs
+++ I4P - interju feladat - 1/Encrypt.cs
@@ -8,6 +8,15 @@ namespace I4P___interju_feladat___1
 {
     internal class Encrypt : Transfer
     {
+        public string EncryptMsg(string msg, string key)
+        {
+            int msgLenght = msg.Length;
+            string encryptedMsg = string.Empty;
 
+            foreach (char c in msg)
+            {
+
+            }
+        }
     }
 }
