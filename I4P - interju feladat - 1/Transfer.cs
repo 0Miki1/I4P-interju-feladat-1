fce29f75d7317ea23cecf2553b324b0c39a208e7
Ábel Miklós <abelmiklos@outlook.hu>
2023-07-21 00:19:27 +0200

    Base method for char conversion

--- I4P - interju feladat - 1/Transfer.cs
+++ I4P - interju feladat - 1/Transfer.cs
@@ -8,6 +8,24 @@ namespace I4P___interju_feladat___1
 {
     internal abstract class Transfer
     {
-        
+        protected int GetCharCode(char character)
+        {
+            if (character == ' ')
+            {
+                return 26;
+            }
+
+            return character - 'a';
+        }
+
+        protected char GetChar(int code)
+        {
+            if (code == 26)
+            {
+                return ' ';
+            }
+
+            return (char)('a' + code);
+        }
     }
 }
