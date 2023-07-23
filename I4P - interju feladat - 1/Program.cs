8f3d7fd2a236a1b9892976d190231b99b8928558
Ábel Miklós <abelmiklos@outlook.hu>
2023-07-23 12:57:49 +0200

    Message encrypter function with console

--- I4P - interju feladat - 1/Program.cs
+++ I4P - interju feladat - 1/Program.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace I4P___interju_feladat___1
 {
     internal class Program
     {
+        private static bool MsgFormat(string msg)
+        {
+            Regex msgReg = new Regex(@"^([a-z]| )+$");
+
+            return msgReg.IsMatch(msg);
+        }
+
+        private static void MsgEncryption()
+        {
+            string msg = string.Empty;
+            string key = string.Empty;
+            Encrypt encrypter = new Encrypt();
+            bool msgOk = true;
+
+            do
+            {
+
+                Console.Write("Add meg a titkosítandó üzenetet: ");
+                msg = Console.ReadLine();
+
+                Console.Write("Add meg a kulcsot az üzenethez: ");
+                key = Console.ReadLine();
+
+                if (!MsgFormat(msg) || !MsgFormat(key))
+                {
+                    Console.WriteLine("\n Helytelen a megadott kulcs vagy üzenet formátuma, próbáld újra!");
+                    msgOk = false;
+                }
+
+                if (msg.Length > key.Length)
+                {
+                    Console.WriteLine("A kulcsnak leglább olyan hosszúnak kell lennie mint az üzenetnek!");
+                    msgOk = false;
+                }
+            } while (!msgOk);
+
+            Console.WriteLine($"A titkosított üzenet: {encrypter.EncryptMsg(msg, key)}");
+        }
+
         static void Main(string[] args)
         {
-
+            MsgEncryption();
           
             Console.ReadKey();
         }
